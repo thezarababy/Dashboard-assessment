ebb5ea2e94d7294f605e57a8c87645672abd01ac
thezarababy <jyokoduwa@gmail.com>
2026-01-09 07:19:05 +0100

    added font-poppins to body in index.css and updated Sidebar and Dashboardlayout components to reflect design changes.

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,22 +1,11 @@
-import Sidebar from "./components/Sidebar";
-import Header from "./components/Navbar";
+import DashboardLayout from "./Layout/Dashboardlayout";
 import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
-    <div className="flex min-h-screen">
-      {/* Sidebar */}
-      <Sidebar />
-
-      {/* Main content */}
-      <div className="flex flex-col flex-1">
-        <Header />
-
-        <main className="p-6">
-          <Dashboard />
-        </main>
-      </div>
-    </div>
+    <DashboardLayout>
+      <Dashboard />
+    </DashboardLayout>
   );
 }
 
